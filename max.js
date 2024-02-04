887e01b478eec424f4e8bbaa59b3bb7e037b658d
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-02-04 20:29:59 +0600

    22-1 Can You Find Out Who Will Get The Delicious Cake?

---
+++ max.js
@@ -0,0 +1,14 @@
+// get max number between 
+
+function getMax(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    else {
+        return num2;
+    }
+}
+
+const max = getMax(50, 500);
+
+console.log(max)
---
+++ max3.js
@@ -0,0 +1,20 @@
+const jim = 90;
+const tim = 91;
+const kim = 98;
+
+if (jim > tim && jim > kim) {
+    console.log('Jim is the ultimate boss');
+}
+else if (tim > jim && tim > kim) {
+    console.log('Tim is a Boss');
+}
+else {
+    console.log('Kim is the Boos')
+}
+
+
+function maxOfThree(num1, num2, num3) {
+    //do something
+}
+
+// console.log(maxOfThree())
