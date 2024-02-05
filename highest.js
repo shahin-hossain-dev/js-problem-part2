bfaf506cb8fabab67352ae3c840ca25371ee55a0
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-02-05 17:16:56 +0600

    22-5 Calculate The Total Cost Of The Products In A Shopping Cart

---
+++ highest.js
@@ -0,0 +1,26 @@
+/**
+ * find highest price from objects
+ */
+
+const phones = [
+    { name: 'samsung', price: 20000, camera: '45px', color: 'black' },
+    { name: 'xaomi', price: 18000, camera: '45px', color: 'black' },
+    { name: 'Oppo', price: 30000, camera: '45px', color: 'black' },
+    { name: 'iphone', price: 100000, camera: '45px', color: 'black' },
+    { name: 'walton', price: 31000, camera: '45px', color: 'black' },
+    { name: 'htc', price: 27000, camera: '45px', color: 'black' },
+]
+
+function getHighest(phones) {
+    let highest = phones[0];
+
+    for (const phone of phones) {
+        if (phone.price > highest.price) {
+            highest = phone;
+        }
+    }
+    return highest;
+}
+
+const result = getHighest(phones);
+console.log(result)
